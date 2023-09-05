e32380e1b9d9ddc5df55e1009bff7a776cde1a70
Brindhakavi <sbrindhakavi@gmail.com>
2023-09-06 08:15:45 +1200

    Sep 6 usestate using function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import "./align.css";
-import { useState } from "react";
-export default function App()
-{
-    const [num,setnum] = useState(3);
 
-return (
-    <div classname = "num">
+ import "./align.css"; import { useState } from "react";
+export default function App()
+ {
+     const [num,setnum] = useState(3);
+
+ return (
+    <div className = "num">
          num{num}
-        <div>
-            <button onClick={()=>setnum(num*2)}>*</button>
+         <div>
+              <button onClick={()=>setnum(num*2)}>*</button>
             <button onClick = {()=>setnum(num/2)}>/</button>
         </div>
     </div>
-);
+)
 }
+
