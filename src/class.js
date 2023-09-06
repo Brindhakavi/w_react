10858220d91d008c46dbd180a5c462ac76fac6bf
Brindhakavi <sbrindhakavi@gmail.com>
2023-09-06 21:43:46 +1200

    sep 6 usestate using class

---
+++ src/class.js
@@ -0,0 +1,38 @@
+import "./align.css";
+import React from "react";
+
+class ClassEx extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            num: 5
+        };
+    }
+    render(){
+        return(
+            <>
+       Num{this.state.num}
+        <div>
+             <button 
+             onClick={()=>
+             this.setState({
+                num : this.state.num * 3
+             })
+            }
+            >multiplication of 3
+                </button>
+                <button 
+             onClick={()=>
+             this.setState({
+                num : this.state.num/2
+             })
+            }
+            >division of 2
+                </button>
+                </div>
+                </>
+                );
+              }
+} 
+export default ClassEx;
+
